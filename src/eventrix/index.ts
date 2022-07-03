87f0a33d2f3f5e7c6f30c7307559126f6e14a4cb
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-07-03 17:05:03 +0200

    Refactored

--- src/eventrix/index.ts
+++ src/eventrix/index.ts
@@ -1,11 +1,30 @@
 import { Eventrix } from 'eventrix';
 import isAuthReceiver from './isAuthReceiver';
+import usersReceiver from './usersReceiver';
+import { AdminEntityResponse, IngredientEntityResponse } from 'types';
+import formBgReceiver from './formBgReceiver';
+import ingredientsReceiver from './ingredientsReceiver';
 
-const initialState = {
+interface State {
+    isAuth: null | boolean;
+    users: null | AdminEntityResponse[];
+    ingredients: null | IngredientEntityResponse[];
+    addForm: boolean;
+}
+
+const initialState: State = {
     isAuth: null,
+    users: null,
+    ingredients: null,
+    addForm: false,
 };
 
-const eventsReceiver = [isAuthReceiver];
+const eventsReceiver = [
+    isAuthReceiver,
+    usersReceiver,
+    ingredientsReceiver,
+    ...formBgReceiver,
+];
 
 const eventrixStore = new Eventrix(initialState, eventsReceiver);
 
