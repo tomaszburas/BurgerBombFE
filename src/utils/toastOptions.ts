eb6993de401fd5eabc057e9cf6dd38ddc8fdb57c
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-07-14 13:05:16 +0200

    Moved favicon files

---
+++ src/utils/toastOptions.ts
@@ -0,0 +1,9 @@
+export const toastOptions = {
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    isLoading: false,
+};
